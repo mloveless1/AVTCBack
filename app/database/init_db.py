554b0ced173b317dd6b967fbcf0b6a5e8625ec64
mloveless1 <malcolmloveless@gmail.com>
2024-01-05 13:35:24 -0800

    ensure correct order of table creation

--- app/database/init_db.py
+++ app/database/init_db.py
@@ -8,16 +8,19 @@ if os.path.exists('.env'):
 
 database_uri = os.getenv('DATABASE_URL')
 
-
 def setup_database():
     engine = create_engine(database_uri)
     # Create an inspector to check for tables
     inspector = inspect(engine)
 
-    # Only create tables that don't already exist
-    for table_name in Base.metadata.tables.keys():
+    # Define the order of table creation
+    ordered_tables = ['parents', 'athletes']
+
+    # Only create tables that don't already exist, in the specified order
+    for table_name in ordered_tables:
         if not inspector.has_table(table_name):
-            Base.metadata.tables[table_name].create(engine)
+            table = Base.metadata.tables[table_name]
+            table.create(engine)
 
 
 if __name__ == "__main__":
