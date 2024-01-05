af8a965396c95bbda068491a408b013518d3b9ca
mloveless1 <malcolmloveless@gmail.com>
2024-01-04 17:44:19 -0800

    Fixed db issue

--- app/database/init_db.py
+++ app/database/init_db.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
-from .base import Base
+from sqlalchemy import create_engine, inspect
+from .base import Base  # Make sure this imports all your models
 import os
 from dotenv import load_dotenv
 
@@ -10,7 +10,13 @@ database_uri = os.getenv('DATABASE_URL')
 
 def setup_database():
     engine = create_engine(database_uri)
-    Base.metadata.create_all(engine)
+    # Create an inspector to check for tables
+    inspector = inspect(engine)
+
+    # Only create tables that don't already exist
+    for table_name in Base.metadata.tables.keys():
+        if not inspector.has_table(table_name):
+            Base.metadata.tables[table_name].create(engine)
 
 
 if __name__ == "__main__":
