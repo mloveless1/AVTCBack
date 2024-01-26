5b62ce7586f19040ecb1964ef2dadd35b1dac00a
mloveless1 <malcolmloveless@gmail.com>
2024-01-26 10:57:56 -0800

    Adjusted CORS

--- app/app.py
+++ app/app.py
@@ -1,17 +1,16 @@
-from flask import Flask, jsonify
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 
-from .database import init_db
-from flask_cors import CORS
+from .database import db
 from .resources import AthleteResource, ParentResource
 from .resources import SignupResource
-from .database import db
-import os
-from dotenv import load_dotenv
 
 if os.path.exists('.env'):
     load_dotenv()
-
 
 database_uri = os.getenv('DATABASE_URL')
 
@@ -19,7 +18,7 @@ app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # to suppress a warning message
-CORS(app, resources={r"/*": {"origins": "https://avtc-signup-front-aa5da244bd4a.herokuapp.com/"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 db.init_app(app)
@@ -42,6 +41,7 @@ api.add_resource(SignupResource, '/signup')
 
 api.add_resource(ParentResource, '/parent/<int:parent_id>')
 
+
 @app.after_request
 def after_request(response):
     header = response.headers
@@ -50,4 +50,3 @@ def after_request(response):
     header['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
     header['Access-Control-Allow-Credentials'] = 'true'
     return response
-
