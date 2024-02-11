65d6f1a9026c6fe34cd05c2d091c5b7f7681c10d
mloveless1 <malcolmloveless@gmail.com>
2024-02-10 18:27:35 -0800

    Updated mail creds

--- app/app.py
+++ app/app.py
@@ -27,8 +27,8 @@ def create_app():
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['MAIL_USERNAME'] = 'malcolmloveless@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'jcch wzuz wblr bhtl'
 
     CORS(app, resources={r"/*": {"origins": "*"}})
     mail = Mail(app)
