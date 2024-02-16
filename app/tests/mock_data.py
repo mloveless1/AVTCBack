c3f99f1130e80a6fd34f5a6436ac640925dff32e
mloveless1 <malcolmloveless@gmail.com>
2024-02-16 07:06:14 -0800

    added some of the tests to the repo to start set up of testing stage in pipeline

---
+++ app/tests/mock_data.py
@@ -0,0 +1,14 @@
+VALID_SIGNUP_DATA = {
+    'parentName': 'John Doe',
+    'email': 'john@example.com',
+    'phoneNumber': '1234567890',
+    'athletes': [
+        {
+            'athleteFullName': 'Jane Doe',
+            'dateOfBirth': '2010-05-01',
+            'gender': 'female',
+            'returner_status': True,
+        }
+    ],
+    'signature': 'data:image/png;base64,...',
+}
