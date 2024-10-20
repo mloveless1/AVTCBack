d81a90f49bd3d67b1a65a90a3d1735b381a423df
mloveless1 <malcolmloveless@gmail.com>
2024-10-19 17:47:50 -0700

    Update mail config to use gmail instead of mailer-to-go

--- app/config/config.py
+++ app/config/config.py
@@ -1,7 +1,6 @@
 import os
 from datetime import timedelta
 
-
 class Config:
     # Celery and JWT Configs
     CELERY_BROKER_URL = os.getenv('REDIS_URL')
@@ -9,21 +8,21 @@ class Config:
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
 
-    # Mailer-To-Go SMTP configuration
-    MAIL_SERVER = os.getenv('MAILERTOGO_SMTP_HOST', 'smtp.us-west-1.mailertogo.net')
-    MAIL_PORT = int(os.getenv('MAILERTOGO_SMTP_PORT', 587))
+    # Gmail SMTP configuration
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = os.getenv('MAILERTOGO_SMTP_USER')
-    MAIL_PASSWORD = os.getenv('MAILERTOGO_SMTP_PASSWORD')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
     # More Email configs
-    NOTIFICATION_EMAIL_RECEIVERS = os.getenv('NOTIFICATION_EMAIL_RECEIVERS')
-    NOTIFICATION_EMAIL_SENDER = f'{MAIL_USERNAME}@{MAIL_SERVER}'
+    NOTIFICATION_EMAIL_RECEIVERS = os.getenv('NOTIFICATION_EMAIL_RECEIVERS', '').split(',')
+    NOTIFICATION_EMAIL_SENDER = MAIL_USERNAME
 
     # SQLAlchemy configs
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQL_ALCHEMY_TRACK_MODIFICATIONS = True
 
     # Year the season will be in
-    SEASON_YEAR = os.getenv('SEASON_YEAR')
+    SEASON_YEAR = os.getenv('SEASON_YEAR', '2025')
